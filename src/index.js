aa8ca2ab17494368945c105ec0dd86f66dd6214c
Anton Severinenko <antonsever@yandex.ru>
2020-11-05 18:06:45 +0400

    Исправление багов

--- src/index.js
+++ src/index.js
@@ -48,7 +48,6 @@ const cardList = new CardList(placesList, createCard, sendPicture);
 const createPopupNew = new PopupForm(popupNew, formNew);
 const createPopupEdit = new PopupForm(popupEdit, formEdit);
 const createPopupImage = new PopupImage(popupPicture);
-//const userInfo = new UserInfo(jobInfo, nameInfo, formEdit, api);
 const userInfo = new UserInfo(jobInfo, nameInfo, jobInfo.textContent, nameInfo.textContent);
 const formValidator = new FormValidator(formNew);
 const formValidatorWithError = new FormValidatorWithError(formEdit);
@@ -61,17 +60,10 @@ api.getUserInfo().then(res => {
 
 function openUserInfo() {
   createPopupEdit.open();
-  //userInfo.setUserInfo();
   formEdit.elements.name.value = nameInfo.textContent;
   formEdit.elements.job.value = jobInfo.textContent;
 }
-/*
-function setUserInfo(event) {
-  event.preventDefault();
-  userInfo.updateUserInfo();
-  createPopupEdit.close();
-}
-*/
+
 function setUserInfo(event) {
   event.preventDefault();
   api.updateUserInfo(formEdit.name.value, formEdit.job.value)
@@ -98,34 +90,3 @@ formEdit.addEventListener('submit', setUserInfo);
 api.getCards().then(res => {
   cardList.loadCards(res.slice(0, 10));
 });
-
-
-/**
- * Привет! У вас получилась очень хорошая и аккуратная работа, обязательный функционал реализован и
- * работает в соответствии с проектным заданием без очевидных багов.
- *
- * Что понравилось:
- *  - Порядок в коде.
- *  - Корректная работа с асинхронным кодом.
- *
- * Что надо исправить для того, чтобы работа была принята:
- *  - Добавляйте catch в конец цепочки промисов для обработки непредвиденных ошибок, например, выводите
- *    текст ошибки в консоль. +++
- *
- * Что можно сделать лучше:
- *  - По-моему, в данный момент класс UserInfo несколько перегружен ответственностями, и лучше бы его
- *    немного упростить, подробнее см. комментарий в userInfo.js
- */
-
-/**
- * 9-ый спринт, 2-ая итерация.
- * Отлично, основная часть комментариев проработана и исправлена, осталось совсем немного.
- * Чтобы работа была принята, исправьте, пожалуйста, все замечания в коде, отмеченные как "Надо исправить".
- * Также обратите внимание на обновленный комментарий "Можно лучше" в файле userInfo.js
- */
-
-/**
- * 9-ый спринт, 3-я итерация.
- * Все критичные замечания исправлены - отличная работа!
- * Успехов на следущих спринтах ;)
- */
